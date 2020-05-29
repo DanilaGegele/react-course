67224e60f8340894c689436ddc586fae132aa693
GegeleDanila <gegeledanila@gmail.com>
2020-05-29 11:52:01 +0300

    add useEffect, onScroll, header logic

--- src/Pages/PageHome/PageHome.js
+++ src/Pages/PageHome/PageHome.js
@@ -4,6 +4,7 @@ function PageHome () {
   return (
     <div>
       <h1>Главная страница</h1>
+      <div style={{ height: '2000px' }}>Очень высокий блок</div>
     </div>
   )
 }
--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,15 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState, useRef } from 'react'
 import Container from 'react-bootstrap/Container'
 import Navbar from 'react-bootstrap/Navbar'
 import { Link } from 'react-router-dom'
+import css from './Header.module.scss'
+import classNames from 'classnames'
 
 function Header () {
+  const lastScrollTop = useRef(0)
+  const [isFixed, setIsFixed] = useState(true)
+  const [isFixedOnTop, setIsFixedOnTop] = useState(true)
+  useEffect(() => {
+    const onScroll = () => {
+      const top = window.pageYOffset
+      if (lastScrollTop.current > top && !isFixed) {
+        setIsFixed(true)
+      } else if (lastScrollTop.current <= top && top > 100 && isFixed) {
+        setIsFixed(false)
+      }
+
+      if (top === 0 && !isFixedOnTop) {
+        setIsFixedOnTop(true)
+      } else if (isFixedOnTop) {
+        setIsFixedOnTop(false)
+      }
+
+      lastScrollTop.current = top
+    }
+
+    window.addEventListener('scroll', onScroll)
+    return () => {
+      window.removeEventListener('scroll', onScroll)
+    }
+  }, [isFixed, isFixedOnTop])
   return (
-    <Container>
-      <Navbar bg='light'>
-        <Navbar.Brand><Link to='/'>Книга рецептов</Link></Navbar.Brand>
-      </Navbar>
-    </Container>
+    <div className={css.header}>
+      <div
+        className={classNames(css.wrap, {
+          [css._fixed]: isFixed,
+          [css._fixed_on_top]: isFixedOnTop
+        })}
+      >
+        <Container>
+          <Navbar bg='light'>
+            <Navbar.Brand><Link to='/'>Книга рецептов</Link></Navbar.Brand>
+          </Navbar>
+        </Container>
+      </div>
+    </div>
   )
 }
 
