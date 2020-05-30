aef6627e0552a33592c61c24e2ab3f116c2ae3ad
GegeleDanila <gegeledanila@gmail.com>
2020-05-30 12:15:54 +0300

    complete homework

--- src/Pages/PagePopUpWithYoutube/PagePopUpWithYoutube.js
+++ src/Pages/PagePopUpWithYoutube/PagePopUpWithYoutube.js
@@ -1,8 +1,21 @@
 import React, { useState } from 'react'
 import Button from 'react-bootstrap/Button'
-import Modal from 'react-bootstrap/Modal'
-import YouTube from 'react-youtube'
-import css from './PagePopUpWithYoutube.module.scss'
+import Modal from 'react-modal'
+import ReactPlayer from 'react-player'
+
+const customStyles = {
+  overlay: {
+    zIndex: 1000
+  },
+  content: {
+    top: '50%',
+    left: '50%',
+    right: 'auto',
+    bottom: 'auto',
+    marginRight: '-50%',
+    transform: 'translate(-50%, -50%)'
+  }
+}
 
 function PagePopUpWithYoutube () {
   const [show, setShow] = useState(false)
@@ -10,25 +23,27 @@ function PagePopUpWithYoutube () {
   const handleClose = () => setShow(false)
   const handleShow = () => setShow(true)
 
-  const opts = {
-    height: '390',
-    width: '640',
-    playerVars: {
-      // https://developers.google.com/youtube/player_parameters
-      autoplay: 0
-    }
-  }
-
   return (
     <>
       <Button variant='primary' onClick={handleShow}>
         Launch demo modal
       </Button>
 
-      <Modal show={show} onHide={handleClose} dialogClassName={css.modal}>
-        <Modal.Body>
-          <YouTube videoId='GLuAIloEWRY' opts={opts} />
-        </Modal.Body>
+      <Modal
+        isOpen={show}
+        onRequestClose={handleClose}
+        style={customStyles}
+      >
+        <ReactPlayer
+          url='https://youtu.be/GLuAIloEWRY'
+          width={640}
+          height={390}
+          config={{
+            youtube: {
+              playerVars: { autoplay: 0 }
+            }
+          }}
+        />
       </Modal>
     </>
   )
