0ea6ffebee5581096e9cb7c0b24ddf3ea7c3d180
Danila <gegeledanila@gmail.com>
2020-06-08 11:38:57 +0300

    complete homework

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -4,14 +4,30 @@ import 'swiper/css/swiper.min.css'
 import css from './Gallery.module.scss'
 import PropTypes from 'prop-types'
 import Image from './Image/Image'
+import Video from './Video/Video'
+import Youtube from './Youtube/Youtube'
 
 function Gallery ({ data }) {
-  const renderSlide = (item, key) => {
-    return (
-      <div>
-        <Image key={key} img={item.img} />
-      </div>
-    )
+  const renderSlide = ({ type, img, video }, key) => {
+    if (type === 'video') {
+      return (
+        <div key={key}>
+          <Video video={video} />
+        </div>
+      )
+    } else if (type === 'youtube') {
+      return (
+        <div key={key}>
+          <Youtube video={video} />
+        </div>
+      )
+    } else if (type === 'img') {
+      return (
+        <div key={key}>
+          <Image img={img} />
+        </div>
+      )
+    }
   }
 
   return (
