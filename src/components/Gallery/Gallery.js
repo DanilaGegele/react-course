d637f901a70742a8646627125730c2e4d9eed382
Danila <gegeledanila@gmail.com>
2020-06-08 12:45:19 +0300

    swiper slider with custom navigation

--- src/components/Gallery/Gallery.js
+++ src/components/Gallery/Gallery.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Swiper from 'react-id-swiper'
 import 'swiper/css/swiper.min.css'
 import css from './Gallery.module.scss'
@@ -6,8 +6,26 @@ import PropTypes from 'prop-types'
 import Image from './Image/Image'
 import Video from './Video/Video'
 import Youtube from './Youtube/Youtube'
+import Button from 'react-bootstrap/Button'
+import ButtonGroup from 'react-bootstrap/ButtonGroup'
 
 function Gallery ({ data }) {
+  const [swiper, setSwiper] = useState(null)
+  const [isBeginning, setIsBeginning] = useState(true)
+  const [isEnd, setIsEnd] = useState(false)
+
+  const goNext = () => {
+    if (swiper !== null) {
+      swiper.slideNext()
+    }
+  }
+
+  const goPrev = () => {
+    if (swiper !== null) {
+      swiper.slidePrev()
+    }
+  }
+
   const renderSlide = ({ type, img, video }, key) => {
     if (type === 'video') {
       return (
@@ -30,11 +48,35 @@ function Gallery ({ data }) {
     }
   }
 
+  const params = {
+    navigation: {
+      nextEl: `.${css.next}`,
+      prevEl: `.${css.prev}`
+    },
+    renderPrevButton: () => <Button className={css.prev}>Prev</Button>,
+    renderNextButton: () => <Button className={css.next}>Next</Button>,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true
+    },
+    on: {
+      slideChangeTransitionStart: function () {
+        const { isBeginning, isEnd } = this
+        setIsBeginning(isBeginning)
+        setIsEnd(isEnd)
+      }
+    }
+  }
+
   return (
     <div className={css.wrapper}>
-      <Swiper>
+      <Swiper getSwiper={setSwiper} {...params}>
         {data.map((item, key) => renderSlide(item, key))}
       </Swiper>
+      <ButtonGroup>
+        <Button onClick={goPrev} variant={isBeginning ? 'secondary' : 'primary'}>Prev</Button>
+        <Button onClick={goNext} variant={isEnd ? 'secondary' : 'primary'}>Next</Button>
+      </ButtonGroup>
     </div>
   )
 }
