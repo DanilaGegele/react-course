53b90e40c390ecbcb51ba55d215726343a21654d
batyr <batyrqh@protonmail.com>
2020-07-29 15:05:31 +0600

    pageList with RDP

--- src/Pages/PageList/PageList.js
+++ src/Pages/PageList/PageList.js
@@ -1,49 +1,29 @@
-import React, { useState } from 'react'
-import data from './assets/data.js'
+import React from 'react'
 import ListGroup from 'react-bootstrap/ListGroup'
-import Button from 'react-bootstrap/Button'
 import { Link } from 'react-router-dom'
+import { RemoteDataProvider } from '@aic/react-remote-data-provider'
+
+const options = {
+  request: {
+    url: 'pageList.json'
+  },
+  reducerKey: 'PageListRDP'
+}
 
 function PageList () {
-  const { list: dataList } = data
-  const [list, setList] = useState(dataList)
-
-  const renderButton = () => {
-    if (list.length > 4) {
-      return (
-        <Button
-          as={Link}
-          to='/'
-        >
-          Посмотреть все новости
-        </Button>
-      )
-    } else {
-      return (
-        <Button
-          onClick={() => {
-            setList([...list, ...dataList])
-          }}
-        >
-          Показать еще
-        </Button>
-      )
-    }
-  }
-
   return (
-    <>
-      <h1>Список</h1>
-      <ListGroup>
-        {list.map(({ title, description }, key) => (
-          <ListGroup.Item key={key} as={Link} to='/detail'>
-            <h3>{title}</h3>
-            <div>{description}</div>
-          </ListGroup.Item>
-        ))}
-      </ListGroup>
-      {renderButton()}
-    </>
+    <RemoteDataProvider {...options}>
+      {({ response: { list } }) => (list).map(({ title, description }, index) => {
+        return (
+          <React.Fragment key={index}>
+            <ListGroup.Item key={index} as={Link} to='/detail'>
+              <h3>{title}</h3>
+              <div>{description}</div>
+            </ListGroup.Item>
+          </React.Fragment>
+        )
+      })}
+    </RemoteDataProvider>
   )
 }
 
